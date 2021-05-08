146c16bcb9771f7fb6c61f87cd36b324af140226
EDUARDO DE OLIVEIRA SILVA <edumapri@gmail.com>
2021-05-08 14:20:02 -0300

    JS/Fundamentos - Nomes dos arq 031 e 032 alterados e adição de 033 - aritmeticos.js

---
+++ 033 - aritmeticos.js
@@ -0,0 +1,16 @@
+// const criada a partir de uma desctructuring de um vetor[]
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c +d;
+const subtracao = d - b;
+const multiplicacao = a * b;
+const divisao = d / a;
+const modulo = a % 2;
+
+console.log("Soma = ", soma);// Saída: 24
+console.log("Subtração = ", subtracao);// Saída: 10
+console.log("Multiplicação = ", multiplicacao);// Saída: 15
+console.log("Divisão = ", divisao);// Saída: 5
+console.log("Módulo = ", modulo);// Saída: 1
+
+
