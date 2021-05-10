367552170d44462e755d1dfc689d2d750d932e62
EDUARDO DE OLIVEIRA SILVA <edumapri@gmail.com>
2021-05-10 17:08:31 -0300

    001 - variaveis.js

--- Curso de Javascript/aula.js
+++ Curso de Javascript/aula.js
@@ -1,5 +1,5 @@
-// let idade = 5;
-// console.log(idade);
-// let altura = 1.70;
-// console.log(altura);
+let idade = 5;
+console.log(idade);
+let altura = 1.70;
+console.log(altura);
 // camelCase
