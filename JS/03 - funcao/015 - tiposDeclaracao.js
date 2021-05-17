d0a6c62227559a7b40783e171d8297d603c666d5
EDUARDO DE OLIVEIRA SILVA <edumapri@gmail.com>
2021-05-17 13:30:44 -0300

    015 - tiposDeclaracao.js

---
+++ JS/03 - funcao/015 - tiposDeclaracao.js
@@ -0,0 +1,18 @@
+console.log(soma(3,4));// Saída: 7
+
+// function declaration
+function soma(x, y) {
+    return x + y;
+}
+
+// function expression
+const sub = function (x, y) {
+    return x -y;
+} 
+console.log(sub(3, 4));// Saída: -1
+
+// named function expression
+const mult = function mult(x, y) {
+    return x * y;
+}
+console.log(mult(3, 4));// Saída: 12
