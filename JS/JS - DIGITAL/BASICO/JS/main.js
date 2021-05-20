8aee4ff55fa5ab9391a6ca525157824f781fffd2
Eduardo de Oliveira Silva <edumapri@gmail.com>
2021-05-19 21:41:14 -0300

    update

---
+++ JS/JS - DIGITAL/BASICO/JS/main.js
@@ -0,0 +1,13 @@
+var nome = "Eduardo de Oliveira Silva";
+var n1 = 5;
+var n2 = 2;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(n1 * n2);
+//console.log(frase.replace.("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
