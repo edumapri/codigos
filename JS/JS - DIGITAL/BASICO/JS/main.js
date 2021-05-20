f634d6d7e8f9f7cd76158fac22a5ad99d8c3360f
Eduardo <edumapri@gmail.com>
2021-05-20 13:56:43 -0300

    atualização main.js

--- JS/JS - DIGITAL/BASICO/JS/main.js
+++ JS/JS - DIGITAL/BASICO/JS/main.js
@@ -1,13 +1,21 @@
-var nome = "Eduardo de Oliveira Silva";
-var n1 = 5;
-var n2 = 2;
-var frase = "Japão é o melhor time do mundo";
+
+//var lista = ["maça","pera","laranja"];// Saída: [ 'maça', 'pera', 'laranja', 'uva' ]
+//lista.push("uva");// Adiciona uva no final do vetor
+//lista.pop();// Remove o último elemento do vetor
+//console.log(lista);// Saída: [ 'maça', 'pera', 'laranja', 'uva' ]
+//console.log(lista.toString());// Saída: maça,pera,laranja,uva
+//console.log(lista.join(" - "));// Saída: maça - pera - laranja - uva
+
+//var nome = "Eduardo de Oliveira Silva";
+//var n1 = 5;
+//var n2 = 2;
+//var frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade + " anos");
 //alert(idade + idade2);
-console.log(nome);
-console.log(n1 + n2);
-console.log(n1 * n2);
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(n1 * n2);
 //console.log(frase.replace.("Japão", "Brasil"));
-console.log(frase.toUpperCase());
-console.log(frase.toLowerCase());
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
 //alert(frase.replace("Japão", "Brasil"));
